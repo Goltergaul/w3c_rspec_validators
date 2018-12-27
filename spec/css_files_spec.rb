779b8d5c36d85422e140fc982cfae3a3aa7e22f5
Alexander Ross <alex@aross.se>
2018-12-27 17:06:29 +0100

    Fix broken test for CssFiles.compiled
    The test did not pass. The expected outcome was incorrect.
    Also updates to new rspec syntax.

--- spec/css_files_spec.rb
+++ spec/css_files_spec.rb
@@ -2,17 +2,23 @@ require "w3c_rspec_validators"
 
 describe "css_files" do
   describe "self.compiled" do
-  
+
     before(:each) do
-      Object.should_receive(:system).with "rake -q assets:precompile"
-      Rails.stub(:root).and_return "railsroot"
-      FileUtils.should_receive(:rm_rf).with "railsroot/public/assets/"
+      expect(Object).to receive(:system).with("rake -q assets:precompile")
+      expect(Rails).to receive(:root).and_return(Pathname.new("railsroot"))
+      expect(FileUtils).to receive(:rm_rf).with("railsroot/public/assets/")
     end
 
     it "should read css files from a directory" do
       directory = File.dirname(__FILE__)
-      W3cRspecValidators::CssFiles.compiled(["#{directory}/fixtures/*.css"])
-        .should == "#home {}\nbody {\n}\n"
+
+      compiled = W3cRspecValidators::CssFiles.compiled(["#{directory}/fixtures/*.css"])
+
+      expect(compiled).to eq <<~CSS
+      body {
+      }
+      #home {}
+      CSS
     end
   end
 end
